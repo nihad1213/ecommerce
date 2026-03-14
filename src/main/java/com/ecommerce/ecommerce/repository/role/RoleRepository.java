991267d045359ed44b7e0b0b1eba4e58a53dbca0
nihad1213 <nihad.nemetli@gmail.com>
2026-03-14 22:28:50 +0400

    new: Repository interface created

--- src/main/java/com/ecommerce/ecommerce/repository/role/RoleRepository.java
+++ src/main/java/com/ecommerce/ecommerce/repository/role/RoleRepository.java
@@ -1,5 +1,11 @@
 package com.ecommerce.ecommerce.repository.role;
 
-public class RoleRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ecommerce.ecommerce.entity.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
     
 }
