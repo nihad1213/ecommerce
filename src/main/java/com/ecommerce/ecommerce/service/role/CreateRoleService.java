9250241225a2fe48b55c6656e96c205693e3c0a1
nihad1213 <nihad.nemetli@gmail.com>
2026-03-16 22:46:22 +0400

    new: Create role Service created

--- src/main/java/com/ecommerce/ecommerce/service/role/CreateRoleService.java
+++ src/main/java/com/ecommerce/ecommerce/service/role/CreateRoleService.java
@@ -1,5 +1,25 @@
 package com.ecommerce.ecommerce.service.role;
 
+import org.springframework.stereotype.Service;
+
+import com.ecommerce.ecommerce.dtos.role.request.CreateRoleRequestDto;
+import com.ecommerce.ecommerce.dtos.role.response.CreateRoleResponseDto;
+import com.ecommerce.ecommerce.entity.Role;
+import com.ecommerce.ecommerce.repository.role.RoleRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class CreateRoleService {
     
+    private final RoleRepository roleRepository;
+
+    public CreateRoleResponseDto createRole(CreateRoleRequestDto dto) {
+        Role role = new Role();
+        role.setName(dto.getName());
+
+        roleRepository.save(role);
+        return new CreateRoleResponseDto("Role created successfully!");
+    }
 }
