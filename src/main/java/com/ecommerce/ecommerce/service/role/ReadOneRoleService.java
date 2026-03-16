c17077d351045ab9bac5c8a33f4598e66136a31a
nihad1213 <nihad.nemetli@gmail.com>
2026-03-16 23:03:46 +0400

    new: ReadOneRole Service created

--- src/main/java/com/ecommerce/ecommerce/service/role/ReadOneRoleService.java
+++ src/main/java/com/ecommerce/ecommerce/service/role/ReadOneRoleService.java
@@ -1,5 +1,27 @@
 package com.ecommerce.ecommerce.service.role;
 
+import org.springframework.stereotype.Service;
+
+import com.ecommerce.ecommerce.dtos.common.request.ReadOneDataRequestDto;
+import com.ecommerce.ecommerce.dtos.role.response.RoleDto;
+import com.ecommerce.ecommerce.entity.Role;
+import com.ecommerce.ecommerce.repository.role.RoleRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class ReadOneRoleService {
     
+    private final RoleRepository roleRepository;
+
+    public RoleDto readOne(ReadOneDataRequestDto dto) {
+        Role role = roleRepository.findById(dto.getId())
+            .orElseThrow(() -> new RuntimeException("Role not found!"));
+        
+        return new RoleDto(
+                role.getId(),
+                role.getName()
+        );
+    }
 }
