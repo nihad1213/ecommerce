9cca4b00e59ae7053d06b6e55fa1a7683f630957
nihad1213 <nihad.nemetli@gmail.com>
2026-03-16 22:49:52 +0400

    new: Delete Service createdf

--- src/main/java/com/ecommerce/ecommerce/service/role/DeleteRoleService.java
+++ src/main/java/com/ecommerce/ecommerce/service/role/DeleteRoleService.java
@@ -1,5 +1,26 @@
 package com.ecommerce.ecommerce.service.role;
 
+import org.springframework.stereotype.Service;
+
+import com.ecommerce.ecommerce.dtos.common.request.DeleteDataRequestDto;
+import com.ecommerce.ecommerce.dtos.common.response.DeleteDataResponseDto;
+import com.ecommerce.ecommerce.entity.Role;
+import com.ecommerce.ecommerce.repository.role.RoleRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class DeleteRoleService {
     
+    private final RoleRepository roleRepository;
+
+    public DeleteDataResponseDto delete(DeleteDataRequestDto dto) {
+        Role role = roleRepository.findById(dto.getId())
+                .orElseThrow(() -> new RuntimeException("Role not found"));
+
+        roleRepository.delete(role);
+
+        return new DeleteDataResponseDto("Role deleted successfully!");
+    }
 }
