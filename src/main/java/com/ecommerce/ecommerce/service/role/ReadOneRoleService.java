520215c88aa95f7356b2009beafc6e73a503208b
nihad1213 <nihad.nemetli@gmail.com>
2026-03-19 22:35:30 +0400

    upd: ReadOneRoleService updated for permission side

--- src/main/java/com/ecommerce/ecommerce/service/role/ReadOneRoleService.java
+++ src/main/java/com/ecommerce/ecommerce/service/role/ReadOneRoleService.java
@@ -1,5 +1,8 @@
 package com.ecommerce.ecommerce.service.role;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 import com.ecommerce.ecommerce.dtos.common.request.ReadOneDataRequestDto;
@@ -20,8 +23,13 @@ public class ReadOneRoleService {
             .orElseThrow(() -> new RuntimeException("Role not found!"));
         
         return new RoleDto(
-                role.getId(),
-                role.getName()
+            role.getId(),
+            role.getName(),
+            role.getPermissions() == null ? Set.of() :
+            role.getPermissions()
+                .stream()
+                .map(p -> p.getPermissionName())
+                .collect(Collectors.toSet())
         );
     }
 }
