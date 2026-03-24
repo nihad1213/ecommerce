596df92f4ed18277475c00a544ed7cf7e621d02e
nihad1213 <nihad.nemetli@gmail.com>
2026-03-24 14:04:39 +0400

    upd: ProductCategoryRepo updated

--- src/main/java/com/ecommerce/ecommerce/repository/productcategory/ProductCategoryRepository.java
+++ src/main/java/com/ecommerce/ecommerce/repository/productcategory/ProductCategoryRepository.java
@@ -1,5 +1,7 @@
 package com.ecommerce.ecommerce.repository.productcategory;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.ecommerce.ecommerce.entity.ProductCategory;
 @Repository
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long>{
     boolean existsByName(String name);
+
+    Page<ProductCategory> findByNameContainingIgnoreCase(String name, Pageable pageable);
 }
--- src/main/java/com/ecommerce/ecommerce/service/productcategory/ReadAllProductCategoryService.java
+++ src/main/java/com/ecommerce/ecommerce/service/productcategory/ReadAllProductCategoryService.java
@@ -1,5 +1,20 @@
 package com.ecommerce.ecommerce.service.productcategory;
 
+import org.springframework.stereotype.Service;
+
+import com.ecommerce.ecommerce.dtos.productcategory.request.ReadAllProductCategoryRequestDto;
+import com.ecommerce.ecommerce.dtos.productcategory.response.ReadAllProductCategoriesResponseDto;
+import com.ecommerce.ecommerce.repository.productcategory.ProductCategoryRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class ReadAllProductCategoryService {
-    
+
+    private final ProductCategoryRepository productCategoryRepository;
+
+    public ReadAllProductCategoriesResponseDto readAll(ReadAllProductCategoryRequestDto dto) {
+        
+    }
 }
