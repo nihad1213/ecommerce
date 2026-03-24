4088fab4847587dc710416789a5fb88402d7cbd4
nihad1213 <nihad.nemetli@gmail.com>
2026-03-24 13:58:16 +0400

    new: Create Service created

--- src/main/java/com/ecommerce/ecommerce/service/productcategory/CreateProductCategoryService.java
+++ src/main/java/com/ecommerce/ecommerce/service/productcategory/CreateProductCategoryService.java
@@ -1,5 +1,28 @@
 package com.ecommerce.ecommerce.service.productcategory;
 
+import org.springframework.stereotype.Service;
+
+import com.ecommerce.ecommerce.dtos.productcategory.request.CreateProductCategoryRequestDto;
+import com.ecommerce.ecommerce.dtos.productcategory.response.CreateProductCategoryResponseDto;
+import com.ecommerce.ecommerce.entity.ProductCategory;
+import com.ecommerce.ecommerce.repository.productcategory.ProductCategoryRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class CreateProductCategoryService {
     
+    private final ProductCategoryRepository productCategoryRepository;
+
+    public CreateProductCategoryResponseDto create(CreateProductCategoryRequestDto dto) {
+        if (productCategoryRepository.existsByName(dto.getName()))
+            throw new RuntimeException("Product Category with this name already exists!");
+        
+        ProductCategory productCategory = new ProductCategory();
+        productCategory.setName(dto.getName());
+        productCategoryRepository.save(productCategory);
+
+        return new CreateProductCategoryResponseDto("Product category created successfully!");
+    }
 }
