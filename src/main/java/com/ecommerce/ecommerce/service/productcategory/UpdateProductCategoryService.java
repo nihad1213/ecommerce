921503327d32c9d6dddc43c78d6c421eb896a24b
nihad1213 <nihad.nemetli@gmail.com>
2026-03-24 14:09:30 +0400

    new: Update Service created

--- src/main/java/com/ecommerce/ecommerce/service/productcategory/UpdateProductCategoryService.java
+++ src/main/java/com/ecommerce/ecommerce/service/productcategory/UpdateProductCategoryService.java
@@ -1,5 +1,33 @@
 package com.ecommerce.ecommerce.service.productcategory;
 
+import org.springframework.stereotype.Service;
+
+import com.ecommerce.ecommerce.dtos.productcategory.request.UpdateProductCategoryRequestDto;
+import com.ecommerce.ecommerce.dtos.productcategory.response.UpdateProductCategoryResponseDto;
+import com.ecommerce.ecommerce.entity.ProductCategory;
+import com.ecommerce.ecommerce.repository.productcategory.ProductCategoryRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class UpdateProductCategoryService {
-    
+
+    private final ProductCategoryRepository productCategoryRepository;
+
+    public UpdateProductCategoryResponseDto update(UpdateProductCategoryRequestDto dto) {
+
+        ProductCategory category = productCategoryRepository.findById(dto.getId())
+                .orElseThrow(() -> new RuntimeException("Product category not found!"));
+
+        if (!category.getName().equals(dto.getName()) &&
+                productCategoryRepository.existsByName(dto.getName())) {
+            throw new RuntimeException("Product category with this name already exists");
+        }
+
+        category.setName(dto.getName());
+        productCategoryRepository.save(category);
+
+        return new UpdateProductCategoryResponseDto("Product category updated successfully");
+    }
 }
