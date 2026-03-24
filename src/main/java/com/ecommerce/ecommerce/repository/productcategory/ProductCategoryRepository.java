f66ffbfac4b0ff00c9a996a57a56a5b2b071669b
nihad1213 <nihad.nemetli@gmail.com>
2026-03-24 13:54:06 +0400

    upd: method added to Repository

--- src/main/java/com/ecommerce/ecommerce/repository/productcategory/ProductCategoryRepository.java
+++ src/main/java/com/ecommerce/ecommerce/repository/productcategory/ProductCategoryRepository.java
@@ -1,5 +1,11 @@
 package com.ecommerce.ecommerce.repository.productcategory;
 
-public class ProductCategoryRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ecommerce.ecommerce.entity.ProductCategory;
+
+@Repository
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long>{
+    boolean existsByName(String name);
 }
