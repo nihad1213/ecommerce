0e390878030efcbbb32414443cfb418e34c8f0b9
nihad1213 <nihad.nemetli@gmail.com>
2026-03-24 13:42:04 +0400

    new: Service and Repo created

---
+++ src/main/java/com/ecommerce/ecommerce/repository/productcategory/ProductCategoryRepository.java
@@ -0,0 +1,5 @@
+package com.ecommerce.ecommerce.repository.productcategory;
+
+public class ProductCategoryRepository {
+    
+}
---
+++ src/main/java/com/ecommerce/ecommerce/service/productcategory/UpdateProductCategoryService.java
@@ -0,0 +1,5 @@
+package com.ecommerce.ecommerce.service.productcategory;
+
+public class UpdateProductCategoryService {
+    
+}
